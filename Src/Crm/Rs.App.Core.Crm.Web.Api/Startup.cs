ba36d5c25bffed2da28da00a4ed4d758fc188f6c
rajenshrestha@hotmail.com <rajenshrestha@hotmail.com>
2020-01-20 18:58:13 +1100

    Added log4net, need to work on db logging

--- Src/Crm/Rs.App.Core.Crm.Web.Api/Startup.cs
+++ Src/Crm/Rs.App.Core.Crm.Web.Api/Startup.cs
@@ -51,10 +51,11 @@ namespace Rs.App.Core.Crm.Web.Api
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddLog4Net();
             if (env.IsDevelopment())
-            {
+            {                
                 app.UseDeveloperExceptionPage();
             }
             else
